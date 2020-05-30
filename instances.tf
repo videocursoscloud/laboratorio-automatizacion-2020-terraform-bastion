f79a865ef42fbd63beee6b445fac801e2d9c8099
Your Name <you@example.com>
2020-05-30 02:55:15 +0100

    asdf

--- instances.tf
+++ instances.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "bastion" {
   ami           = data.aws_ami.bastion.id
   instance_type = "t2.micro"
   subnet_id = data.terraform_remote_state.vpc.outputs.public_1_subnet_id
-  key_name = aws_key_pair.test-keypair-bastion.key_name
+  key_name = "test-keypair-bastion"
   vpc_security_group_ids = [ aws_security_group.allow_bastion.id ]
   tags = {
     Name = "bastion"
